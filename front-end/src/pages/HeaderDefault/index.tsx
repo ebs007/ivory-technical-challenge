5817081dc24c1077d69d2f928c658944c9cf3d1b
Erick <erickbarretodasilva@outlook.com>
2021-05-28 02:12:05 -0300

    some log removed

--- front-end/src/pages/HeaderDefault/index.tsx
+++ front-end/src/pages/HeaderDefault/index.tsx
@@ -20,7 +20,6 @@ const Header: React.FC = () => {
         },
       })
       .then(response => {
-        console.log(response.data)
         setList(response.data)
       })
       .catch(function (error) {
